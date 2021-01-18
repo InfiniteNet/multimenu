dbf6a3beb9d3746848049264f57472dc9794c068
fabricio94b <60627225+fabricio94b@users.noreply.github.com>
2021-01-18 20:35:25 -0300

    Update multi.sh

--- multi.sh
+++ multi.sh
@@ -1,12 +1,4 @@
-#!/bin/bash
-#====================================================
-#	SCRIPT: MULTI SCRIPTS MANAGER
-#   DATA ATT:   16 de Ene 2020
-#	DESENVOLVIDO POR:	TEAM ILLUMINATI
-#	COLABORADOR:	        JONY RIVERA
-#	CONTATO TELEGRAM:	NO DISPONIBLE
-#	CANAL TELEGRAM:	https://t.me/admmanagerfree
-#====================================================
+
 barra="\033[0m\e[34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo "/root/multi.sh" > /bin/mko && chmod +x /bin/mko > /dev/null 2>&1
 
@@ -14,45 +6,21 @@ echo "/root/multi.sh" > /bin/mko && chmod +x /bin/mko > /dev/null 2>&1
 
 ##TEAM-ILUUMINATI
 
-painelv20 () {
+VPSMANAGER () {
      apt-get update -y; apt-get upgrade -y; wget https://raw.githubusercontent.com/fabricio94b/VPS-MANAGER/main/Plus; chmod 777 Plus; ./Plus
 }
-admmanageralpha () {
+PAINELV20 () {
     wget https://raw.githubusercontent.com/fabricio94b/Painel-v20/main/install; chmod +x install; ./install chmod 777 instala.sh* && ./instala.sh*
 }
-newultimate () {
+BOTFREE () {
     apt-get update -y; apt-get upgrade -y; wget https://raw.githubusercontent.com/fabricio94b/botfree/main/iniciar.sh -O iniciar.sh; chmod +x iniciar.sh; ./iniciar.sh chmod 777 instalar.sh* && ./instalar.sh*
-}
-vpspack20 () {
-    apt-get update -y; apt-get upgrade -y; wget https://raw.githubusercontent.com/fabricio94b/PROYECTOS_DESCONTINUADOS/master/VPSPACK-SERVER-V2.0/instalarold && bash instalarold
-}
-dankel () {
-    apt-get update -y; apt-get upgrade -y; wget https://raw.githubusercontent.com/fabricio94b/PROYECTOS_DESCONTINUADOS/master/ADM-MANAGER-DANKELTHAHER/instala.sh; chmod 777 instala.sh* && ./instala.sh*
-}
-scriptmx_illuminati () {
-    sudo apt update -y; apt upgrade -y; wget https://raw.githubusercontent.com/fabricio94b/PROYECTOS_DESCONTINUADOS/master/NEW-ULTIMATE-VPS-MX-8.0/instalscript.sh; chmod 777 instalscript.sh* && ./instalscript.sh*
-}
-
-##OPCIONES DE SISTEMA
-
-atualizar () {
-    rm -rf $HOME/multi.sh; wget https://raw.githubusercontent.com/fabricio94b/MULTI/main/multi.sh; chmod +x multi.sh; ./multi.sh
-}
-remove_multiscripts () {
-    rm -rf $HOME/multi.sh
-}
-
-##TESTER SCRIPTS
-
-scriptadmmx_reapergamo1test () {
-    apt-get update -y; apt-get upgrade -y; wget https://raw.githubusercontent.com/casitadelterror/scripts/master/gamo/instalador; chmod 777 instalador* && ./instalador*; passwd
 }
 
 while true $x != "ok"
 do
 clear
 echo -e "$barra"
-echo -e "\E[41;1;37m             MULTI-SCRIPTS MANAGER   \033[1;32m[\033[1;37mV: 1.0\033[1;32m]\E[0m"
+echo -e "\E[41;1;37m            MULTI-SCRIPTS MANAGER   \033[1;32m[\033[1;37mV: 1.0\033[1;32m]\E[0m"
 echo -e "$barra"
 echo -e "
 \033[37m------------  \033[1;30m[ \033[37m MULTI-SCRIPTS \033[1;30m]\033[37m  --------------
@@ -68,32 +36,17 @@ echo -ne "\033[1;32mOQUE DESEJA FAZER \033[1;33m?\033[1;31m?\033[1;37m : "; read
 case "$x" in 
    1 | 01)
    clear
-   painelv20
+   VPSMANAGER
    exit;
    ;;
    2 | 02)
    clear
-   admmanageralpha
+   PAINELV20
    exit;
    ;;
    3 | 03)
    clear
-   newultimate
-   exit;
-   ;;
-   4 | 04)
-   clear
-   vpspack20
-   exit;
-   ;;      
-   5 | 05)
-   clear
-   dankel
-   exit;
-   ;;
-   6 | 06)
-   clear
-   scriptmx_illuminati
+   BOTFREE
    exit;
    ;;
    0 | 00)
