9f981fa3f2b17d015d9d5495cfcb66164ff50ccf
InfiniteNet <117833353+InfiniteNet@users.noreply.github.com>
2024-01-16 16:41:34 -0300

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -19,7 +19,7 @@ HABILITARROOT () {
     wget -y; bash <(wget -qO- https://raw.githubusercontent.com/InfiniteNet/Abilitar-Root/main/senharoot.sh)
 }
 BOTWHATSAPP () {
-    sudo apt install -y git && git clone https://github.com/InfiniteNet/whaticket install && sudo chmod -R 777 ./install && cd ./install && sudo ./install_primaria
+    sudo apt install -y git && git clone https://github.com/InfiniteNet/whaticket-instalador.git && sudo chmod -R 777 whaticket-saas && cd whaticket-saas && sudo ./install_primaria
 }    
 ATTSSL () {
     apt install wget -y; wget --no-check-certificate https://www.dropbox.com/scl/fi/5dd563hduerr5uk1d8lph/ssl.zip?rlkey=hj8mu86cxlho9bstr4ej0h34z&dl=0; chmod +x ssl.sh; ./ssl.sh
